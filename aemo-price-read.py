a900f38eec0ff945ab2df1b19f16dfc719fe4f56
Trev <trev@bigtrev.com>
2021-05-11 08:16:58 +1000

    New commit

--- aemo-price-read.py
+++ aemo-price-read.py
@@ -2,6 +2,7 @@
 # Some info on importing web pages here: https://stackoverflow.com/questions/40879856/how-do-i-get-the-html-of-a-website-using-python-3
 # Other info from another repo for getting data from AEMO here: https://github.com/KaigeWang/web-scraping/blob/master/src/module/webclient.java 
 # This older repo seems to have some info as well: https://github.com/hsenot/aemo-json
+# This code definitely looks like it may be useful: https://github.com/hsenot/aemo-json/blob/master/script/extract-demand-price-30mn.py 
 
 import urllib.request
 
@@ -14,7 +15,7 @@ target_url = "http://www.nemweb.com.au/mms.GRAPHS/GRAPHS/GRAPH_30VIC1.csv" #This
 
 source = urllib.request.urlopen(target_url).read()
 
-# now I need to get the last one with "TRADE" in the column
+# now I need to get the last one with "TRADE" in the column, looks like row 50 (first with PD in it) is the next predicted price for next half hour
 
 print(source)
 
